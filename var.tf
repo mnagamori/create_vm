ed517eeda56a6c11ee4e411c08eb2e7d4fcd7292
mnagamor <mnagamor@cisco.com>
2021-09-29 17:06:33 +0900

    modify

--- var.tf
+++ var.tf
@@ -1,5 +1,5 @@
 variable "vsphere_user" {default = "administrator@vsphere.local"}
 variable "vsphere_password" {default = "Cisco123!"}
 variable "vsphere_server" {default = "9.1.120.1"}
-variable "vm_name" {default = "tfcb_vss_vm1"}
+variable "vm_name" {default = "tfcb-vcs-vm1"}
 #variable "vm_name" {default = "terraform-test"}
